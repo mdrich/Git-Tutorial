8dce0bbe9779ac10531fd82a66b543ddc6980eb3
Michael Rich <mrich@michaels-mac-pro.local>
2018-10-19 15:48:33 -0400

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Michael Rich on 10/19/18.
+//  Copyright © 2018 Michael Rich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
